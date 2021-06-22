0c63ed85abd076615a372304443d25f896106e5b
Paweł Merdała <p.merdala@interia.pl>
2021-06-22 23:42:56 +0200

    Jobs.kt cancel

--- src/main/kotlin/pl/merdala/coroutinesbasics/Jobs.kt
+++ src/main/kotlin/pl/merdala/coroutinesbasics/Jobs.kt
@@ -1,14 +1,19 @@
 package pl.merdala.coroutinesbasics
 
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main(){
     runBlocking {
         val job1: Job = launch {
+            delay(3000L)
             println("Job1 launched")
         }
         job1.invokeOnCompletion { println("Job1 completed") }
+        delay(500L)
+        println("Job1 will be cancelled")
+        job1.cancel()
     }
 }
