ce8c615737828bc9d81bf7b02eabee372211845c
Paweł Merdała <p.merdala@interia.pl>
2021-06-22 23:09:05 +0200

    LightweightCoroutines.kt

---
+++ src/main/kotlin/pl/merdala/coroutinesbasics/LightweightCoroutines.kt
@@ -0,0 +1,14 @@
+package pl.merdala.coroutinesbasics
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(){
+    runBlocking {
+        repeat(1_000_000){
+            launch {
+                print(".")
+            }
+        }
+    }
+}
