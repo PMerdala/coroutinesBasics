161e914b8be4d6c650c0719dfc7f11f8a55e5912
Paweł Merdała <p.merdala@interia.pl>
2021-06-28 18:33:17 +0200

    Jobs.kt cancel

--- src/main/kotlin/pl/merdala/coroutinesbasics/Jobs.kt
+++ src/main/kotlin/pl/merdala/coroutinesbasics/Jobs.kt
@@ -8,11 +8,24 @@ import kotlinx.coroutines.runBlocking
 fun main(){
     runBlocking {
         val job1: Job = launch {
-            delay(3000L)
-            println("Job1 launched")
+//            delay(3000L)
+            println("Job1 is launching")
+
+            val job2: Job = launch{
+                println("Job2 is launching")
+                delay (500L)
+                println("Job2 finished")
+            }
+            job2.invokeOnCompletion { println ("Job2 completed") }
+            val job3: Job = launch{
+                println("Job3 is launching")
+                delay (1500L)
+                println("Job3 finished")
+            }
+            job3.invokeOnCompletion { println ("Job3 completed") }
         }
         job1.invokeOnCompletion { println("Job1 completed") }
-        delay(500L)
+        delay(1000L)
         println("Job1 will be cancelled")
         job1.cancel()
     }
